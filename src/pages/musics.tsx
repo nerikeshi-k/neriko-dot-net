03f58302b3110256184470bfa55a0b0968b3a2d3
nerikeshi-k <nerikeshik@gmail.com>
2019-08-16 17:31:46 +0900

    no border

--- src/pages/musics.tsx
+++ src/pages/musics.tsx
@@ -13,6 +13,7 @@ export default () => (
           height="166"
           scrolling="no"
           allow="autoplay"
+          frameBorder="no"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/466302672&color=%23cbd2d3&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         ></iframe>
       </PlayerWrapper>
@@ -24,6 +25,7 @@ export default () => (
           height="166"
           scrolling="no"
           allow="autoplay"
+          frameBorder="no"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365302955&color=%23cbd2d3&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         ></iframe>
       </PlayerWrapper>
@@ -35,6 +37,7 @@ export default () => (
           height="166"
           scrolling="no"
           allow="autoplay"
+          frameBorder="no"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365298602&color=%23cbd2d3&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         ></iframe>
       </PlayerWrapper>
@@ -47,6 +50,7 @@ export default () => (
           src="https://ext.nicovideo.jp/thumb/sm17213446"
           scrolling="no"
           style={{ border: 'solid 1px #ccc' }}
+          frameBorder="no"
         >
           <a href="https://www.nicovideo.jp/watch/sm17213446">【初音ミク】雪解けの朝へ【オリジナルPV】</a>
         </iframe>
