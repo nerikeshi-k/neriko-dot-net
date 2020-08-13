c8f8f466f156bf84390fad654bb765d6208e7634
nerikeshi-k <nerikeshik@gmail.com>
2020-08-13 14:37:23 +0900

    fix 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,17 @@
+import Heading from 'components/Heading';
 import React from 'react';
 import styled from 'styled-components';
 
 export default () => (
   <Wrapper>
+    <Heading>neriko.net</Heading>
     <Message>ページが見つかりませんでした</Message>
   </Wrapper>
 );
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: auto;
+`;
 
 const Message = styled.div``;
