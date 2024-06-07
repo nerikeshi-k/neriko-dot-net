bc5a32546377f7e07bc8a3e13ef7ffe2a2306dd4
nerikeshi-k <nerikeshik@gmail.com>
2024-06-07 23:07:06 +0900

    remove wrangler

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
+import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths()],
 });
